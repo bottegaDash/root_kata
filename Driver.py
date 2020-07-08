392346cacc02403a38335ed30399fc45cfb79ec2
David Ash <bottegadash@gmail.com>
2020-07-08 14:53:13 -0400

    Added doc string

--- Driver.py
+++ Driver.py
@@ -6,6 +6,12 @@ class Driver:
         self._trips = Trips()
 
     def add_trip(self, *args):
+        """
+        Calls the add_trip method in Trips
+        :param args: an array of parameters that will be passed down to the
+                     last class. typical includes start_time, end_time,
+                     distance_driven
+        """
         self._trips.add_trip(*args)
 
     @property
