7c079c24e2917b234111302307d4231e11ac99cc
David Ash <bottegadash@gmail.com>
2020-07-07 20:25:27 -0400

    Added doc string

--- Trip.py
+++ Trip.py
@@ -1,7 +1,19 @@
 class Trip:
     def __init__(self, start_time, end_time, distance):
+        """
+        The initialization of a Trip Object
+        :param start_time: String: format "00:00"
+        :param end_time: String: format "00:00"
+        :param distance: String: format "1.0"
+        """
         self._distance = float(distance)
+
 
     @property
     def distance(self):
+        """
+        Return the distance of the trip
+
+        :return: distance: Float
+        """
         return self._distance
