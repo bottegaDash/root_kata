617902b62af34e62b499b5435ecfd195694ae9d8
David Ash <bottegadash@gmail.com>
2020-07-08 16:11:51 -0400

    Added doc string

--- Drivers.py
+++ Drivers.py
@@ -3,4 +3,9 @@ from Driver import Driver
 
 class Drivers(dict):
     def add_driver(self, name):
+        """
+        Adds the name of the driver as the key and the Driver object as the value
+        
+        :param name: the name of the drive
+        """
         self.update({name: Driver()})
