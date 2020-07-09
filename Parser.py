9c3feecaddeefb242354f6dd490dfdbe58e1ba20
David Ash <bottegadash@gmail.com>
2020-07-08 20:52:38 -0400

    Added abiltiy to parse file

--- Parser.py
+++ Parser.py
@@ -2,13 +2,21 @@ from Drivers import Drivers
 
 
 def parse_input(input_file):
-    d = Drivers()
+    """
+    Takes in a file and builds a drivers_object based on the commands and parameters
+    passed through
+
+    :param input_file: the file to parse
+    :return: drives_object: the drivers_object
+    """
+    drivers_object = Drivers()
     with open(input_file) as fp:
         for line in fp:
             line = line.strip()
             command = line.split(' ')[0]
             parameters = line.split(' ')[1:]
             if command == "Driver":
-                d.add_driver(*parameters)
+                drivers_object.add_driver(*parameters)
             elif command == "Trip":
-                d.add_trip(*parameters)
+                drivers_object.add_trip(*parameters)
+    return drivers_object
--- testdir/test_parser.py
+++ testdir/test_parser.py
@@ -3,22 +3,27 @@ from unittest.mock import patch, mock_open
 from Parser import parse_input
 
 
+@patch("Parser.Drivers")
 class TestDriver(unittest.TestCase):
 
-    @patch("Parser.Drivers")
     def test_add_driver(self, mock_drivers):
         m = mock_open(read_data="Driver Adam")
         with patch('builtins.open', m):
             parse_input("/a/b/c")
             mock_drivers().add_driver.assert_called_with("Adam")
 
-    @patch("Parser.Drivers")
     def test_add_trip(self, mock_drivers):
         m = mock_open(read_data="""Driver Adam\n
                                    Trip Adam 00:00 01:00 60""")
         with patch('builtins.open', m):
             parse_input("/a/b/c")
             mock_drivers().add_trip.assert_called_with("Adam", "00:00", "01:00", "60")
+
+    def test_get_driver_object(self, mock_drivers):
+        m = mock_open(read_data="")
+        with patch('builtins.open', m):
+            driver_object = parse_input("/a/b/c")
+            self.assertEqual(driver_object, mock_drivers())
 
 
 if __name__ == '__main__':
